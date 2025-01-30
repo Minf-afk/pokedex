d053491bfb04ed5dab70e44dcc0356862d053345
Minf-afk <msantana.inf@gmail.com>
2025-01-30 09:59:53 -0300

    Entendendo API

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+  .then(function (response) {
+    response.json().then(function (responseBody) {
+      console.log(responseBody);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log("fetch completed");
+  });
