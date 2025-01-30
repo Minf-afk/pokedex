1bc55ac146b3773bbfce250b31b3b1f53f418cbf
Minf-afk <msantana.inf@gmail.com>
2025-01-30 15:28:38 -0300

    Adicionando Imagem pela Api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+function pokemonTypeDetails (pokemonTypes){
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 const offset = 0;
 const limit = 10;
 
@@ -5,15 +9,14 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
   return `<li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">#00${pokemon.order}</span>
           <span class="name">${pokemon.name}</span>
           <div class="detail">
             <ol class="types">
-              <li class="type">Grass</li>
-              <li class="type">Poison</li>
+              ${pokemonTypeDetails(pokemon.types).join('')}
             </ol>
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              src="${pokemon.sprites.other.dream_world.front_default}"
               alt="${pokemon.name}"/>
           </div>
         </li>`;
